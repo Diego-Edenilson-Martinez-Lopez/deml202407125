6adcf281692acb72f7d64dc4a59a362f560f0a99
MINEDUCYT <7178497@clases.edu.sv>
2024-07-25 18:24:15 -0600

    se creo multiplicacion

--- src/main/java/com/example/deml20240725/Controllers/CalculadoradosDEMLController.java
+++ src/main/java/com/example/deml20240725/Controllers/CalculadoradosDEMLController.java
@@ -7,27 +7,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping("/calculadora")
-public class CalculadoraDEMLController {
+public class CalculadoradosDEMLController {
     
-    @GetMapping("/resta")
-    public String showRestaForm(Model model) {
+    @GetMapping("/multiplicacion")
+    public String showMultiplicationForm(Model model) {
         model.addAttribute("num1", 0);
         model.addAttribute("num2", 0);
         model.addAttribute("result", 0);
-        return "CalculadoraDEML/restademl";
+        return "CalculadoraDEML/multiplicaciondeml";
     }
 
-    @PostMapping("/resta")
-    public String performResta(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
-        int result = num1 - num2;
+    @PostMapping("/multiplicacion")
+    public String performMultiplication(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+        int result = num1 * num2;
         model.addAttribute("num1", num1);
         model.addAttribute("num2", num2);
         model.addAttribute("result", result);
-        return "CalculadoraDEML/restademl";
-        
+        return "CalculadoraDEML/multiplicaciondeml";
     }
-        
 }
